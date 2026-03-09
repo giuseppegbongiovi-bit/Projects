63d8306532a11ab095075961555c0c280cabc017
giuseppegbongiovi-bit <giuseppegbongiovi@gmail.com>
2026-03-09 18:55:12 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+
+
+def add(x,y):
+    print(x+y)
+    return(x+y)
+    
+def main():
+    a = int(input("first variable: "))
+    b = int(input("second variable: "))
+    add(a,b)
+
+if __name__ == "__main__":
+    main()
